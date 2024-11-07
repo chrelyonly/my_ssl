b8d226b96b3af4c146237dde8288aa03173fcfe0
chrelyonly <1172576293@qq.com>
2024-11-07 11:16:01 +0800

    1

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 	"crypto/x509/pkix"
 	"encoding/json"
 	"encoding/pem"
+	"io/ioutil"
 	"log"
 	"math/big"
 	"net"
@@ -16,132 +17,177 @@ import (
 )
 
 const (
-	RSABitsSize        = 4096
+	RSABitsSize        = 2048
 	CACertFilename     = "build/ca.pem"
 	CAKeyFilename      = "build/ca.key"
-	ConfigFilename     = "build/config.json"
-	ServerCertFilename = "server.pem"
-	ServerKeyFilename  = "server.key"
+	ServerCertFilename = "build/server.pem"
+	ServerKeyFilename  = "build/server.key"
+	ConfigFilename     = "build/config.json" // 配置文件名
 )
 
-// Config 代表 DNS 名称和 IP 地址的配置结构
+// 配置文件的结构体
 type Config struct {
-	DNSNames    []string `json:"dns_names"`
-	IPAddresses []string `json:"ip_addresses"`
+	IPs     []string `json:"ips"`     // 存储IP地址
+	Domains []string `json:"domains"` // 存储域名
+	CA      CAConfig `json:"ca"`      // 存储CA配置
 }
 
-// 生成 RSA 私钥
-func generateRSAKey(bits int) *rsa.PrivateKey {
-	key, err := rsa.GenerateKey(rand.Reader, bits)
-	if err != nil {
-		log.Fatalf("生成 RSA 私钥失败: %v", err)
-	}
-	return key
+type CAConfig struct {
+	Country            string `json:"country"`            // 国家
+	Province           string `json:"province"`           // 省份
+	Organization       string `json:"organization"`       // 组织
+	OrganizationalUnit string `json:"organizationalUnit"` // 组织单位
+	CommonName         string `json:"commonName"`         // 公共名称
+	ValidityYears      int    `json:"validityYears"`      // 有效期（年）
 }
 
-// 保存 PEM 文件
-func savePEMFile(filename, pemType string, data []byte) {
+// 生成RSA私钥
+func GenRsaPK(size int) (*rsa.PrivateKey, error) {
+	return rsa.GenerateKey(rand.Reader, size)
+}
+
+// 生成并保存PEM格式文件
+func savePEM(filename string, blockType string, bytes []byte) error {
 	file, err := os.Create(filename)
 	if err != nil {
-		log.Fatalf("创建文件 %s 失败: %v", filename, err)
+		return err
 	}
 	defer file.Close()
-	if err := pem.Encode(file, &pem.Block{Type: pemType, Bytes: data}); err != nil {
-		log.Fatalf("写入 PEM 块失败: %v", err)
-	}
-	log.Printf("%s 保存成功", filename)
+
+	return pem.Encode(file, &pem.Block{Type: blockType, Bytes: bytes})
 }
 
-// 生成并保存证书
-func generateCertificate(filename, keyFilename string, template, parent *x509.Certificate, pubKey, parentPrivKey interface{}) {
-	certData, err := x509.CreateCertificate(rand.Reader, template, parent, pubKey, parentPrivKey)
+// 从JSON文件加载配置
+func loadConfig(filename string) (*Config, error) {
+	data, err := ioutil.ReadFile(filename)
 	if err != nil {
-		log.Fatalf("生成证书失败: %v", err)
-	}
-	savePEMFile(filename, "CERTIFICATE", certData)
-
-	if privKey, ok := parentPrivKey.(*rsa.PrivateKey); ok {
-		savePEMFile(keyFilename, "RSA PRIVATE KEY", x509.MarshalPKCS1PrivateKey(privKey))
-	}
-}
-
-// 生成并保存 CA 证书
-func generateCACertificate() (*x509.Certificate, *rsa.PrivateKey) {
-	caKey := generateRSAKey(RSABitsSize)
-	now := time.Now()
-	caTemplate := &x509.Certificate{
-		SerialNumber:          big.NewInt(now.UnixNano()),
-		Subject:               pkix.Name{Country: []string{"CN"}, Organization: []string{"chrelyonly"}},
-		NotBefore:             now,
-		NotAfter:              now.AddDate(100, 0, 0),
-		KeyUsage:              x509.KeyUsageDigitalSignature | x509.KeyUsageCertSign | x509.KeyUsageDataEncipherment,
-		ExtKeyUsage:           []x509.ExtKeyUsage{x509.ExtKeyUsageClientAuth, x509.ExtKeyUsageServerAuth},
-		BasicConstraintsValid: true,
-		IsCA:                  true,
-		SubjectKeyId:          md5.New().Sum([]byte(now.String())),
-	}
-	generateCertificate(CACertFilename, CAKeyFilename, caTemplate, caTemplate, &caKey.PublicKey, caKey)
-	return caTemplate, caKey
-}
-
-// 生成并保存服务器证书
-func generateServerCertificate(ca *x509.Certificate, caKey *rsa.PrivateKey, config Config) {
-	serverKey := generateRSAKey(RSABitsSize)
-	now := time.Now()
-	serverTemplate := &x509.Certificate{
-		SerialNumber:   big.NewInt(now.UnixNano()),
-		Subject:        pkix.Name{Country: []string{"CN"}, Organization: []string{"chrelyonly"}, CommonName: "chrelyonly Server"},
-		NotBefore:      now,
-		NotAfter:       now.AddDate(100, 0, 0),
-		KeyUsage:       x509.KeyUsageDigitalSignature | x509.KeyUsageKeyEncipherment,
-		ExtKeyUsage:    []x509.ExtKeyUsage{x509.ExtKeyUsageServerAuth},
-		DNSNames:       config.DNSNames,
-		AuthorityKeyId: ca.SubjectKeyId,
-	}
-
-	// 解析 IP 地址字符串
-	for _, ipStr := range config.IPAddresses {
-		if ip := net.ParseIP(ipStr); ip != nil {
-			serverTemplate.IPAddresses = append(serverTemplate.IPAddresses, ip)
-		} else {
-			log.Printf("无效 IP 地址: %s", ipStr)
-		}
-	}
-
-	generateCertificate(ServerCertFilename, ServerKeyFilename, serverTemplate, ca, &serverKey.PublicKey, caKey)
-}
-
-// 从配置文件加载 DNS 名称和 IP 地址
-func loadConfig(filename string) Config {
-	file, err := os.ReadFile(filename)
-	if err != nil {
-		log.Fatalf("读取配置文件 %s 失败: %v", filename, err)
+		return nil, err
 	}
 
 	var config Config
-	if err := json.Unmarshal(file, &config); err != nil {
-		log.Fatalf("解析配置文件失败: %v", err)
+	if err := json.Unmarshal(data, &config); err != nil {
+		return nil, err
 	}
-	return config
+
+	return &config, nil
 }
 
-// 加载 CA 证书和私钥
-func loadCA(certFile, keyFile string) (*x509.Certificate, *rsa.PrivateKey) {
+// 生成CA证书和私钥
+func generateCA(caConfig CAConfig) (*x509.Certificate, *rsa.PrivateKey, error) {
+	key, err := GenRsaPK(RSABitsSize)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	// CA证书信息
+	caInfo := pkix.Name{
+		Country:            []string{caConfig.Country},
+		Province:           []string{caConfig.Province},
+		Organization:       []string{caConfig.Organization},
+		OrganizationalUnit: []string{caConfig.OrganizationalUnit},
+		CommonName:         caConfig.CommonName,
+	}
+
+	// CA证书配置
+	ca := &x509.Certificate{
+		SerialNumber:          big.NewInt(time.Now().UnixNano()),
+		Subject:               caInfo,
+		NotBefore:             time.Now(),
+		NotAfter:              time.Now().AddDate(caConfig.ValidityYears, 0, 0), // 使用配置中的有效期
+		KeyUsage:              x509.KeyUsageCertSign | x509.KeyUsageDigitalSignature,
+		ExtKeyUsage:           []x509.ExtKeyUsage{x509.ExtKeyUsageServerAuth, x509.ExtKeyUsageClientAuth},
+		IsCA:                  true,
+		BasicConstraintsValid: true,
+		SubjectKeyId:          md5.New().Sum([]byte(time.Now().String())),
+	}
+
+	// 使用CA私钥自签名生成证书
+	certBytes, err := x509.CreateCertificate(rand.Reader, ca, ca, &key.PublicKey, key)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	// 保存CA证书和私钥
+	if err := savePEM(CACertFilename, "CERTIFICATE", certBytes); err != nil {
+		return nil, nil, err
+	}
+	if err := savePEM(CAKeyFilename, "RSA PRIVATE KEY", x509.MarshalPKCS1PrivateKey(key)); err != nil {
+		return nil, nil, err
+	}
+
+	return ca, key, nil
+}
+
+// 生成服务器证书
+func generateServerCert(caConfig CAConfig, ca *x509.Certificate, caKey *rsa.PrivateKey, config *Config) error {
+	key, err := GenRsaPK(RSABitsSize)
+	if err != nil {
+		return err
+	}
+	// 服务器证书信息
+	serverInfo := pkix.Name{
+		Country:            []string{caConfig.Country},
+		Province:           []string{caConfig.Province},
+		Organization:       []string{caConfig.Organization},
+		OrganizationalUnit: []string{caConfig.OrganizationalUnit},
+		CommonName:         caConfig.CommonName,
+	}
+
+	// 服务器证书配置
+	serverCert := &x509.Certificate{
+		SerialNumber:   big.NewInt(time.Now().UnixNano()),
+		Subject:        serverInfo,
+		NotBefore:      time.Now(),
+		NotAfter:       time.Now().AddDate(caConfig.ValidityYears, 0, 0), // 1年有效期
+		KeyUsage:       x509.KeyUsageDigitalSignature | x509.KeyUsageKeyEncipherment,
+		ExtKeyUsage:    []x509.ExtKeyUsage{x509.ExtKeyUsageServerAuth},
+		AuthorityKeyId: ca.SubjectKeyId,
+	}
+
+	// 将配置中的 IP 地址添加到服务器证书
+	for _, ip := range config.IPs {
+		serverCert.IPAddresses = append(serverCert.IPAddresses, net.ParseIP(ip))
+	}
+
+	// 将配置中的域名添加到服务器证书
+	for _, domain := range config.Domains {
+		serverCert.DNSNames = append(serverCert.DNSNames, domain)
+	}
+
+	// 使用CA签发服务器证书
+	certBytes, err := x509.CreateCertificate(rand.Reader, serverCert, ca, &key.PublicKey, caKey)
+	if err != nil {
+		return err
+	}
+
+	// 保存服务器证书和私钥
+	if err := savePEM(ServerCertFilename, "CERTIFICATE", certBytes); err != nil {
+		return err
+	}
+	if err := savePEM(ServerKeyFilename, "RSA PRIVATE KEY", x509.MarshalPKCS1PrivateKey(key)); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// LoadCA 从文件加载CA证书和私钥
+func LoadCA(certFile, keyFile string) (*x509.Certificate, *rsa.PrivateKey) {
 	caData := loadPEMData(certFile)
 	ca, err := x509.ParseCertificate(caData.Bytes)
 	if err != nil {
-		log.Fatalf("解析 CA 证书失败: %v", err)
+		log.Fatalf("解析CA证书失败: %v", err)
 	}
 
 	keyData := loadPEMData(keyFile)
 	caKey, err := x509.ParsePKCS1PrivateKey(keyData.Bytes)
 	if err != nil {
-		log.Fatalf("解析 CA 私钥失败: %v", err)
+		log.Fatalf("解析CA私钥失败: %v", err)
 	}
 	return ca, caKey
 }
 
-// 加载 PEM 数据
+// loadPEMData 从文件加载PEM数据
 func loadPEMData(filename string) *pem.Block {
 	data, err := os.ReadFile(filename)
 	if err != nil {
@@ -152,15 +198,19 @@ func loadPEMData(filename string) *pem.Block {
 }
 
 func main() {
-	// 生成 CA 证书
-	// caCert, caKey := generateCACertificate()
+	// 读取配置文件
+	config, err := loadConfig(ConfigFilename)
+	if err != nil {
+		log.Fatalf("加载配置失败: %v", err)
+	}
 
-	// 加载现有的 CA 证书和私钥
-	caCert, caKey := loadCA(CACertFilename, CAKeyFilename)
+	// 使用配置文件中的 CA 配置生成 CA 证书和私钥
+	//ca, caKey, _ := generateCA(config.CA)
+	ca, caKey := LoadCA(CACertFilename, CAKeyFilename)
 
-	// 加载配置文件
-	config := loadConfig(ConfigFilename)
-
-	// 生成服务器证书
-	generateServerCertificate(caCert, caKey, config)
+	// 使用配置文件生成服务器证书
+	if err := generateServerCert(config.CA, ca, caKey, config); err != nil {
+		log.Fatalf("生成服务器证书失败: %v", err)
+	}
+	log.Println("服务器证书和私钥生成成功")
 }
